1653cd598ce7ad5d9beece6832737c45c9be754d
victormdsm <victor.de.souza.muller@gmail.com>
2024-10-03 11:59:57 -0300

    find

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -5,10 +5,9 @@ import com.example.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -22,7 +21,25 @@ public class UsuarioController {
         try {
             return new ResponseEntity<>(usuarioService.save(usuario), HttpStatus.CREATED);
         } catch (Exception e){
-            throw new RuntimeException("Erro ao criar o usuario");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Usuario>> findAll () {
+        try {
+            return new ResponseEntity<>(usuarioService.findAll(), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/findbyid/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id){
+        try {
+            return new ResponseEntity<>(usuarioService.findById(id), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/com/example/demo/service/UsuarioService.java
+++ src/main/java/com/example/demo/service/UsuarioService.java
@@ -4,6 +4,9 @@ import com.example.demo.entities.Usuario;
 import com.example.demo.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -11,8 +14,21 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Transactional
     public Usuario save(Usuario usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario findById(Long id){
+        var user = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException());
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
     }
 
 }
